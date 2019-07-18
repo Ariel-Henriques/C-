ac9528668607f0b84fa3474e4355687a9144af46
Ariel-Henriques <48598433+Ariel-Henriques@users.noreply.github.com>
2019-07-18 16:52:54 -0300

    Create recFatorial.cpp

---
+++ recFatorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int fatorial(int);
+
+int fatorial(int n){
+//returna fatorial de n
+	if (n==0){
+		return 1;
+	}else{
+		return (n * fatorial(n-1));
+	}
+}
+
+int main (void){
+	int n, k;
+	cout << "Escreve ae um numero: ";
+	cin >> n;
+	k = fatorial(n);
+	cout << "Fatorial de "<< n << " = " << k <<'\n';
+	return 0;
+} 
